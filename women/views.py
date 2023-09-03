ad3af863e844087aea8bc2720dd9834f5f12406d
Azat Yanberdin <azatdjon@yandex.ru>
2023-09-03 23:21:25 +0500

    add new changes

--- women/views.py
+++ women/views.py
@@ -6,5 +6,13 @@ def index(request):
     return HttpResponse("Страница приложения women")
 
 
-def categories(request):
-    return HttpResponse("<h1>Статьм по категориям</h>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьм по категориям</h><p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьм по категориям</h><p>slug: {cat_slug}</p>")
+
+
+def archieve(request, year):
+    return HttpResponse(f"<h1>Архив по годам: </h><p>year: {year}</p>")
